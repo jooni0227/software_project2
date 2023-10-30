be1e0d2ce396dda78ddc822bd4a701b5622b4104
jooni0227 <115866788+jooni0227@users.noreply.github.com>
2023-10-30 20:15:14 +0900

    Update TabContainer.js

--- navigation/TabContainer.js
+++ navigation/TabContainer.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import {View,Text,StyleSheet,Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { FontAwesome } from '@expo/vector-icons';
 
 import Home from './menu/Home';
 import Login from './menu/Login';
 import Vote from './menu/Vote';
-import SignUp from './menu/SignUp';
 
 const home="home";
 const login="로그인";
