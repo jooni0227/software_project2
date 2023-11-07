57ce3dd286c01f66db0afcaabf890669a19ad03b
jooni0227 <115866788+jooni0227@users.noreply.github.com>
2023-11-07 17:03:42 +0900

    Update TabContainer.js

--- navigation/TabContainer.js
+++ navigation/TabContainer.js
@@ -6,7 +6,7 @@ import Home from './menu/Home';
 import Login from './menu/Login';
 import Vote from './menu/Vote';
 
-const home="home";
+const home="Home";
 const login="로그인";
 const vote="투표";
 
@@ -15,9 +15,9 @@ const Tab=createBottomTabNavigator();
 export default function TabContainer(){
     return (
         <Tab.Navigator
-            initialRouteName={login}>
-            <Tab.Screen name={home} component={Home} options={{}}/>
+            initialRouteName={home}>
             <Tab.Screen name={vote} component={Vote} options={{}} />
+            <Tab.Screen name={home} component={Home} options={{}}/>
             <Tab.Screen name={login} component={Login} options={{}}/>
         </Tab.Navigator>
     );
